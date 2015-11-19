238b08402a2c7464fbfdf9e14590b8c2b24aeab7
Huang Yaming <yumminhuang@gmail.com>
2015-11-19 00:15:38 -0500

    Update PairsOccurrenceReducer by emitting total count also

--- src/main/java/analysis/count/PairsOccurrenceReducer.java
+++ src/main/java/analysis/count/PairsOccurrenceReducer.java
@@ -1,9 +1,10 @@
 package analysis.count;
 
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.mapreduce.Reducer;
-
 import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 
 /**
  * @Author: yummin
@@ -11,8 +12,16 @@ import java.io.IOException;
  */
 public class PairsOccurrenceReducer extends Reducer<YearAttributePair, IntWritable, YearAttributePair, IntWritable> {
 
-    private IntWritable totalCount = new IntWritable();
-    private IntWritable currentYear = new IntWritable(0);
+    private IntWritable totalCount;
+    private IntWritable currentYear;
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        super.setup(context);
+        // Initialize variables
+        totalCount = new IntWritable();
+        currentYear = new IntWritable(0);
+    }
 
     @Override
     protected void reduce(YearAttributePair key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
@@ -20,6 +29,7 @@ public class PairsOccurrenceReducer extends Reducer<YearAttributePair, IntWritab
             if (key.year.equals(currentYear))
                 totalCount.set(totalCount.get() + getTotalCount(values));
             else {
+                // Reset variables
                 currentYear.set(key.year.get());
                 totalCount.set(0);
                 totalCount.set(getTotalCount(values));
@@ -28,6 +38,13 @@ public class PairsOccurrenceReducer extends Reducer<YearAttributePair, IntWritab
             int count = getTotalCount(values);
             context.write(key, new IntWritable(count));
         }
+    }
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        // Emit total count
+        context.write(new YearAttributePair(currentYear, new Text("*")), totalCount);
+        super.cleanup(context);
     }
 
     private int getTotalCount(Iterable<IntWritable> values) {
