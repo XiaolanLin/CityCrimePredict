d0b89f549ccb46581431eb49707b242555715ff1
dtt0123 <dengtiantong@Dengs-MacBook-Pro.local>
2015-11-19 13:50:29 -0500

    changes for merge

--- src/main/java/analysis/ResolvedRate/ResolvedRate.java
+++ src/main/java/analysis/ResolvedRate/ResolvedRate.java
@@ -1,5 +1,7 @@
 package ResolvedRateChange;
 
+import java.io.IOException;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
@@ -8,18 +10,36 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class ResolvedRate {
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-	    Job job = new Job(conf, "Incidents Resolved Rate");
-	    job.setJarByClass(ResolvedRate.class);
+	private Configuration conf;
+	private Job job;
+	private String input;
+	private String output;
+	public ResolvedRate(String input, String output) throws IOException {
+		this.conf = new Configuration();
+	    this.job = new Job(this.conf, "Incidents Resolved Rate");
+	    this.input = input;
+	    this.output = output;
+	    
 	    job.setMapperClass(ResolvedRateMapper.class); 
 	    job.setReducerClass(ResolvedRateReducer.class);
 	    job.setNumReduceTasks(10);
 	    job.setPartitionerClass(PdDistrictPartitioner.class);
+	    job.setMapOutputKeyClass(Text.class);
+	    job.setMapOutputValueClass(Text.class);
 	    job.setOutputKeyClass(Text.class);
 	    job.setOutputValueClass(Text.class);
-	    FileInputFormat.addInputPath(job, new Path(args[0]));
-	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-	    System.exit(job.waitForCompletion(true) ? 0 : 1);
-	  }
+	    FileInputFormat.addInputPath(job, new Path(input));
+	    FileOutputFormat.setOutputPath(job, new Path(output));
+	}
+	public Job getJob() {
+		return job;
+	}
+	public void run() throws InterruptedException, IOException, ClassNotFoundException {
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 }
+
+
+
+
+
