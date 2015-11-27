132af6202963f2c50661b0f7df3b01ebff9eaa2a
Huang Yaming <yumminhuang@gmail.com>
2015-11-27 14:03:27 -0500

    Fix counting month

--- src/main/java/analysis/countByMonth/IncidencePerMonthMapper.java
+++ src/main/java/analysis/countByMonth/IncidencePerMonthMapper.java
@@ -17,20 +17,19 @@ public class IncidencePerMonthMapper extends Mapper<LongWritable, Text, IntWrita
     private CSVParser parser;
 
     @Override
-    protected void setup(Mapper.Context context) throws IOException, InterruptedException {
+    protected void setup(Context context) throws IOException, InterruptedException {
         super.setup(context);
         this.parser = new CSVParser();
     }
 
     @Override
-    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] tokens = this.parser.parseLine(value.toString());
         if (!tokens[0].equals("IncidntNum")) {
             String[] dateSplit = tokens[Constants.INDEX_DATE].split("/");
             int year = Integer.parseInt(dateSplit[2]);
             int month = Integer.parseInt(dateSplit[0]);
-            int newKey = year * 100 + month;
-            context.write(new IntWritable(newKey), one);
+            context.write(new IntWritable(month), one);
             context.write(new IntWritable(year), one);
         }
     }
